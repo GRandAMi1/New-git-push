0cee26f72fa27d736ef1879afd42dec126510a7e
GRandAMi1 <Amitshendge1990@gmailcom>
2021-07-29 21:02:58 +0530

    3rd Branch

--- src/com/blz/git/GitPush.java
+++ src/com/blz/git/GitPush.java
@@ -3,9 +3,9 @@ package com.blz.git;
 public class GitPush {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		System.out.println("Hello New World");
 		System.out.println("Hello Newbr World");
+		System.out.println("Hello new3br World");
 
 	}
 
